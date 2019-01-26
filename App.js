94038f40a694fa2f6a394168b246e879f788252a
SathishSaminathan <Sathishcse1975@gmail.com>
2019-01-26 10:47:34 +0530

    bug-fixes

--- App.js
+++ App.js
@@ -28,8 +28,6 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    this.checkPermission();
-    this.createNotificationListeners();
     SplashScreen.hide();
     this.authSubscription = firebase.auth().onAuthStateChanged(user => {
       this.setState({
@@ -37,88 +35,7 @@ export default class App extends Component {
         user
       });
     });
-  }
-
-  async checkPermission() {
-    const enabled = await firebase.messaging().hasPermission();
-    if (enabled) {
-        this.getToken();
-    } else {
-        this.requestPermission();
-    }
-  }
-
-  async requestPermission() {
-    try {
-        await firebase.messaging().requestPermission();
-        // User has authorised
-        this.getToken();
-    } catch (error) {
-        // User has rejected permissions
-        console.log('permission rejected');
-    }
-  }
-
-  async getToken() {
-    let fcmToken = await AsyncStorage.getItem('fcmToken', value);
-    if (!fcmToken) {
-        fcmToken = await firebase.messaging().getToken();
-        if (fcmToken) {
-            // user has a device token
-            await AsyncStorage.setItem('fcmToken', fcmToken);
-        }
-    }
-  }
-
-  async createNotificationListeners() {
-    /*
-    * Triggered when a particular notification has been received in foreground
-    * */
-    this.notificationListener = firebase.notifications().onNotification((notification) => {
-        const { title, body } = notification;
-        this.showAlert(title, body);
-    });
-  
-    /*
-    * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
-    * */
-    this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
-        const { title, body } = notificationOpen.notification;
-        this.showAlert(title, body);
-    });
-  
-    /*
-    * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
-    * */
-    const notificationOpen = await firebase.notifications().getInitialNotification();
-    if (notificationOpen) {
-        const { title, body } = notificationOpen.notification;
-        this.showAlert(title, body);
-    }
-    /*
-    * Triggered for data only payload in foreground
-    * */
-    this.messageListener = firebase.messaging().onMessage((message) => {
-      //process data message
-      console.log(JSON.stringify(message));
-    });
-  }
-
-  showAlert(title, body) {
-    Alert.alert(
-      title, body,
-      [
-          { text: 'OK', onPress: () => console.log('OK Pressed') },
-      ],
-      { cancelable: false },
-    );
-  }
-
-  componentWillUnmount() {
-    this.authSubscription();
-    this.notificationListener();
-    this.notificationOpenedListener();
-  }
+  }  
 
   _currentUser = CurrentUser => {
     this.setState(
--- android/app/src/main/java/com/mychat/MainApplication.java
+++ android/app/src/main/java/com/mychat/MainApplication.java
@@ -3,6 +3,7 @@ package com.mychat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new SplashScreenReactPackage(),
             new RNGoogleSigninPackage(),
             new RNFirebasePackage(),
